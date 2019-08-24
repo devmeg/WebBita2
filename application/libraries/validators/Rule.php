4a7c7afcfa22ebe76f0ca9c35b5d9b36d883ea4e
Gaston Marsano <gstnmarsano@gmail.com>
2019-08-24 18:07:41 -0400

    Se añade biblioteca de validacion con clase basica de reglas

---
+++ application/libraries/validators/Rule.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rule {
+  protected $value;
+  protected $isValid = false;
+
+  public function setValue($value) {
+    $this->value = $value;
+    $this->evaluate($this->value);
+  }
+
+  /**
+   * Hacer Override del método
+   * evalua la regla sobre `value` y guarda el resultado en `isValid`
+   *
+   * @return void
+   **/
+  protected function evaluate() {
+    $this->isValid = true;
+  }
+
+  public function validate() {
+    return $this->isValid;
+  }
+
+  public function getValue() {
+    return $this->value;
+  }
+}
